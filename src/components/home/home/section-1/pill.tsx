11293057162494a05773d4c6baeadabe5b7d4db2
roy <royokunbor@gmail.com>
2021-11-06 20:54:35 +0100

    converted and exported svg to components

--- src/components/home/home/section-1/pill.tsx
+++ src/components/home/home/section-1/pill.tsx
@@ -2,6 +2,7 @@ import { pillInterface } from "../../../../models/pill";
 import "../../../../styles/home/section-1/_pill.scss";
 import { useState, useEffect } from "react";
 import { headerLinkService } from "../../../../services/headerLinkService";
+import SearchIcon from "../../../../assets/icons/searchIcon";
 export const Pill = () => {
   const pillListItems: pillInterface[] = [
     { title: "Location", subTitle: "Where are you going" },
@@ -48,26 +49,7 @@ export const Pill = () => {
           </p>
         ))}
         <button className="pill-container__search">
-          <svg
-            className="pill-container__search__icon"
-            viewBox="0 0 32 32"
-            xmlns="http://www.w3.org/2000/svg"
-            aria-hidden="true"
-            role="presentation"
-            focusable="false"
-            style={{
-              display: "block",
-              fill: "none",
-              height: "16px",
-              width: "16px",
-              stroke: "currentcolor",
-              overflow: "visible",
-            }}
-          >
-            <g fill="none">
-              <path d="m13 24c6.0751322 0 11-4.9248678 11-11 0-6.07513225-4.9248678-11-11-11-6.07513225 0-11 4.92486775-11 11 0 6.0751322 4.92486775 11 11 11zm8-3 9 9"></path>
-            </g>
-          </svg>
+          <SearchIcon />
           <span className="pill-container__search__word">search</span>
           <span className="pill-container__search__word--small">
             Where are you going
